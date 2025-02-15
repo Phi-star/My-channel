a7d6682c315b8c3c31b9d30d7127a4f1095f8b9b
Phi-star <saliuphilip8@gmail.com>
2025-02-15 22:42:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let image = document.getElementById("redirectImage");
+
+    // Add click event to image
+    image.addEventListener("click", function() {
+        window.location.href = "https://shorturl.at/FUrJH"; // Redirect to WhatsApp
+    });
+});
